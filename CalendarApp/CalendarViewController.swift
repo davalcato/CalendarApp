a485a90940b00e7b16b0b8042cf8dc30ea7343f0
davalcato <davalcato@yahoo.com>
2022-09-28 14:25:19 -0700

    created the eventStore in the CalendarViewController to trigger the popup.

--- CalendarApp/CalendarViewController.swift
+++ CalendarApp/CalendarViewController.swift
@@ -7,15 +7,25 @@
 
 import UIKit
 import CalendarKit
+import EventKit
 
 class CalendarViewController: DayViewController {
+    // create the event store
+    private let eventStore = EKEventStore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // add title
         title = "Calendar"
+        // function reques
+        requestAccessToCalendar()
     }
-
-
+    // function for access
+    func requestAccessToCalendar() {
+        eventStore.requestAccess(
+            to: .event) { success, error in
+            
+        }
+    }
 }
 
