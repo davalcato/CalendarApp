aea9762e015ef224e6e3d95a9e49ae0c999ab1ae
davalcato <davalcato@yahoo.com>
2022-10-03 13:38:53 -0700

    use the eventStore to fetch the dates created by user.

--- CalendarApp/CalendarViewController.swift
+++ CalendarApp/CalendarViewController.swift
@@ -24,8 +24,39 @@ class CalendarViewController: DayViewController {
     func requestAccessToCalendar() {
         eventStore.requestAccess(
             to: .event) { success, error in
-            
         }
+    }
+    // new events for date
+    override func eventsForDate(_ date: Date) -> [EventDescriptor] {
+        // fetch events with date range
+        let startDate = date
+        var oneDayComponents = DateComponents()
+        // the span of the dates
+        oneDayComponents.day = 1
+        // create end date
+        let endDate = calendar.date(byAdding: oneDayComponents, to: startDate)!
+        
+        // fetch all the events from the beginning of startDate to end of date
+        let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+        
+        // fetch from eventstore
+        let eventKitEvents = eventStore.events(matching: predicate)
+        
+        // The `eventKitEvents` has a type of `[EKEvent]`, we need to convert them to CalendarKit Events
+        let calendarKitEvents = eventKitEvents.map { eKEvent -> Event in
+            // separate events
+            let ckEvent = Event() // Creating a new CalendarKit.Event
+//            ckEvent.startDate = eKEvent.startDate
+//            ckEvent.endDate = eKEvent.endDate
+            ckEvent.isAllDay = eKEvent.isAllDay
+            ckEvent.text = eKEvent.description
+            // pass the color
+            if let eventColor = eKEvent.calendar.cgColor {
+                ckEvent.color = UIColor(cgColor: eventColor)
+            }
+            return ckEvent
+        }
+        return calendarKitEvents
     }
 }
 
